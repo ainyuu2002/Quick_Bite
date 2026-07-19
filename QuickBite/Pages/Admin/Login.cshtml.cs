194ef1a9dc345cca04897837e3a0cac799040c1f
ainyuu2002 <phidung2002.work@gmail.com>
2026-07-19 12:41:55 +0700

    [A] implement login handler

--- QuickBite/Pages/Admin/Login.cshtml.cs
+++ QuickBite/Pages/Admin/Login.cshtml.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using QuickBite.Data;
+using QuickBite.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace QuickBite.Pages.Admin
 {
     public class LoginModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public LoginModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
         [BindProperty]
         [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
         public string Username { get; set; } = string.Empty;
@@ -16,6 +30,40 @@ namespace QuickBite.Pages.Admin
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var user = await _db.AdminUsers.FirstOrDefaultAsync(u => u.Username == Username);
+            if (user is null)
+            {
+                ModelState.AddModelError(string.Empty, "Tên đăng nhập hoặc mật khẩu không đúng");
+                return Page();
+            }
+
+            var hasher = new PasswordHasher<AdminUser>();
+            var verifyResult = hasher.VerifyHashedPassword(user, user.PasswordHash, Password);
+
+            if(verifyResult == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError(string.Empty, "Tên đăng nhập hoặc mật khẩu không đúng");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.Name, user.Username),
+                new (ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            return RedirectToPage("/Index");
         }
     }
 }
