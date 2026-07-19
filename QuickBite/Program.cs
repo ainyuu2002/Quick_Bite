e2bbbe26c7f64af3491b8c1a83c0b06db3a08da2
ainyuu2002 <phidung2002.work@gmail.com>
2026-07-19 10:26:18 +0700

    [A] register AppDbContext with SQL Server

--- QuickBite/Program.cs
+++ QuickBite/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using QuickBite.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
